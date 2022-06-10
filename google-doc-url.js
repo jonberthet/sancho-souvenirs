e8af28f75080a3bcd09dfa1b33d7f0722936c157
Jon Berthet <jonberthet@gmail.com>
2022-06-10 00:28:51 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1OdIPYaDd1W74pZikyu5yR0goVbHX0ulAXpjQwV3Owis/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyD2cdIUkgsZ_hDTgoM6Q3JvlPU00z0eMQU';
